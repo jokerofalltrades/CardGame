40295caf7f8572522aed137486a643c828e15a49
Daniel <21williamson@parmiters.herts.sch.uk>
2024-11-28 19:29:20 +0000

    Config Update (NET Version Update)

--- obj/Debug/net8.0/CardGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/CardGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CardGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49d60af6a61be2bb98512f6b1283c97090953037")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f10b215bb6827e77fc358427573fb3199b041ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("CardGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CardGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
